c559fc26908836c4933ceb9fd8f0dff37ca9530b
camron <camronmoradi@gmail.com>
2018-12-24 22:22:49 +0700

    Adds eventlisteners to the DOM in app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
-console.log("Hello Students!");
+document.addEventListener('DOMContentLoaded', () => {
+
+  const addContactForm = document.querySelector('.new-contact-form')
+
+  addContactForm.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const {    // NOTE: this is destructuring, assigning variable names to the corresponding elements
+      name,
+      email,
+      phone,
+      company,
+      notes,
+      twitter,
+    } = addContactForm.elements
+
+    const contact = {   // NOTE: and this creates a hash object, with key:value pairs
+      id: Date.now(),
+      name: name.value,
+      email: email.value,
+      phone: phone.value,
+      company: company.value,
+      notes: notes.value,
+      twitter: twitter.value,
+    }
+
+    console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
+  
+  })
+
+})
